af79b38ac36700813ee5d54f5158394bc2fa7514
marialegl <psico.marialegl@gmail.com>
2023-10-31 10:57:50 -0700

    Print the lowercase alphabet in reverse order

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function generates a random number and determines
+ * whether it's positive, negative, or zero.
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 122; i >= 97; --i)
+	{
+		printf("%c", i);
+	}
+	printf("\n");
+
+	return (0);
+}
