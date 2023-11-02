b46fb774bf31a9d0597c9202d342433fdc1c53de
marialegl <psico.marialegl@gmail.com>
2023-11-01 21:18:58 -0700

    I sometimes suffer from insomnia. And when I can not fall asleep, I play what I call the alphabet game

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * main - c program: print  a line of text
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Maria Alejandra Gonzalez
+ * Desc: Header file containing declarations for all functions
+ *       used in the functions_nested_loops directory.
+ */
+
 int _putchar(char c);
+void print_alphabet(void);
+
+#endif
