13918fc9ce18841a741a9475e58b4d5043bd8e31
marialegl <psico.marialegl@gmail.com>
2023-11-06 10:56:01 -0800

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Maria Alejandra Gonzalez
+ * Desc: Header file containing declarations for all functions
+ *       used in the C - Pointers, arrays and strings.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif       
