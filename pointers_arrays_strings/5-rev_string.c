82357dc8248f717f32e76a968022284e34448583
marialegl <psico.marialegl@gmail.com>
2023-11-08 18:22:42 -0800

    Function that reverses a string.

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,21 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * rev_string - prints a string, makes a line break 
- * and prints the same string in reverse on the same line.
- * @s: pointer to the string.
- * main: print characters whit putchar
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
  */
-int main(void);
 void rev_string(char *s)
 {
-	int len = strlen(s);
-	int i;
+	int len = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		while (*s != '\0')
-		{
-			_putchar(*s);
-			s++;
-		}
-		_putchar('\n');
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
-	for (i = len - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
 }
