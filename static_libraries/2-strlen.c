0da27b199c208a1816be005fa46fb009fc8b5f03
marialegl <psico.marialegl@gmail.com>
2023-11-16 18:18:00 -0800

    Create the static library libmy.a

---
+++ static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer.
+ * Return: cantidad de letras.
+ */
+int _strlen(char *s)
+{
+	int cantidadletras;
+
+	cantidadletras = strlen(s);
+	return (cantidadletras);
+}
