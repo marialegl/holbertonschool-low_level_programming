845b86c685d3fdf6c2cada00a0850cc6ee2f57cb
marialegl <psico.marialegl@gmail.com>
2023-11-17 09:23:34 -0800

    Prints the name of the program.

---
+++ argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Maria Alejandra Gonzalez
+ * Desc: Header file containing declarations for all functions
+ *       used in the functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif
