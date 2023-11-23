9f6466f68d6aad2d79fa082c1ac60fff4275bbfb
marialegl <psico.marialegl@gmail.com>
2023-11-23 11:06:23 -0800

    function that creates a new dog..

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,25 +2,6 @@
 #include <stdio.h>
 #include "dog.h"
 
-/**
- * _strcopy - Copies a string pointed to by src, including the
- *            terminating null byte, to a buffer pointed to by dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- *
- * Return: The pointer to dest.
- */
-char *_strcopy(char *dest, char *src)
-{
-	int index = 0;
-
-	for (index = 0; src[index]; index++)
-		dest[index] = src[index];
-
-	dest[index] = '\0';
-
-	return (dest);
-}
 
 /**
  * new_dog - check description.
@@ -39,9 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dogcute);
 		return (NULL);
 
-	dogcute->name = _strcopy(dogcute->name, name);
-	dogcute->age = age;
-	dogcute->owner = _strcopy(dogcute->owner, owner);
+		dogcute->name = name;
+		dogcute->age = age;
+		dogcute->owner = owner;
 	}
 	return (dogcute);
 }
