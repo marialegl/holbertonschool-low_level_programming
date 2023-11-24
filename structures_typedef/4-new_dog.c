3421d6965809262d9744a2154a7506b09dcbfbd4
marialegl <psico.marialegl@gmail.com>
2023-11-24 04:05:06 -0800

    creates a new dog whit copy of name and owner.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,28 +1,53 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
-
+#include <stdlib.h>
 
 /**
- * new_dog - check description.
- * description: creates a new dog structure.
- * @name: name the new pet.
- * @age: age the new pet.
- * @owner: owner the new pet.
- * Return: returns pointer to buffer of datatype dog.
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: struct dog.
+ * if fails, returns NULL.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dogcute = malloc(sizeof(dog_t));
+	dog_t *p_dog;
+	int i, lname, lowner;
 
-	if (dogcute == NULL)
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
 	{
-		free(dogcute);
+		free(p_dog);
 		return (NULL);
-
-		dogcute->name = name;
-		dogcute->age = age;
-		dogcute->owner = owner;
 	}
-	return (dogcute);
+
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
 }
