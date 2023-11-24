faab6a8bc581eb79decd6d40f916c517ce428147
marialegl <psico.marialegl@gmail.com>
2023-11-24 10:13:03 -0800

    function whit pointer to function that prints a name.

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "function_pointers.h"
+
+/**
+  * print_name - function that prints a name.
+  * @name: pointer to name of the person.
+  * @f: pointer to function.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	(f)(name);
+}
