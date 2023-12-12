3ae7062a2efb411271bcbe9c860d4db3266958cd
marialegl <psico.marialegl@gmail.com>
2023-12-11 18:41:58 -0800

    Taks 0: function that prints all the elements of a list_t list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/*
+   * print_list - prints all the elements of a list_t list
+   * @list_t: data structure
+   * @*h: constant pointer to a structure
+   * Return: memory sizes
+   */
+size_t print_list(const list_t *h)
+{
+	size_t node_count;
+	const list_t *ptr;
+
+	ptr = h;
+	node_count = 0;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+		node_count++;
+	}
+	return (node_count);
+}
