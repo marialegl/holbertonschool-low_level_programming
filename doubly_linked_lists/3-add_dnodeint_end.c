30eddeab6e45c76858fc1e614effeaad5c031c6d
marialegl <psico.marialegl@gmail.com>
2023-12-14 09:29:38 -0800

    Tasks 3:  function that adds a new node at the end of a dlistint_t list. reviewed

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,10 +19,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = NULL;
-	new->prev = *head;
 	if (*head == NULL)
 	{
 		*head = new;
+		new->prev = NULL;
+		return (new);
 	}
 	else
 	{
@@ -32,6 +33,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			p = p->next;
 		}
 		p->next = new;
+		new->prev = p;
 	}
 	return (new);
 }
