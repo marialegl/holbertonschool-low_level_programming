5be6d31091169742e013b82dd6fee63e2f5b8177
marialegl <psico.marialegl@gmail.com>
2023-12-14 19:58:30 -0800

    Tasks 5: returns the nth node of a dlistint linked list.

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -8,14 +7,19 @@
  * @index: index of the node, starting from 0.
  * Return: if the node does not exist, return NULL.
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *aux;
+	unsigned int node_count = 0;
+	dlistint_t *ptr = head;
 
-	while (head != NULL)
+	while (ptr != NULL)
 	{
-		aux = head;
-		head = head->next;
-		free(aux);
+		if (node_count == index)
+		{
+			return (ptr);
+		}
+		ptr = ptr->next;
+		node_count++;
 	}
+	return (NULL);
 }
