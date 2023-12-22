0119222ff25d0d96bccf89aa7b42fae60b17ece8
marialegl <psico.marialegl@gmail.com>
2023-12-22 11:47:20 -0800

    Tasks 3: function that adds an element to the hash table.

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -34,7 +34,6 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/* 0-hash_table_create.c */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
